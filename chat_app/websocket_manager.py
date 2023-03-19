9c7ec7bbed68eac9241b92bbbeb139b12890c237
Kamavaram Satish Kumar <satishkamavaram@gmail.com>
2023-03-19 15:53:36 +0530

    Update websocket_manager.py

--- chat_app/websocket_manager.py
+++ chat_app/websocket_manager.py
@@ -1,9 +1,13 @@
 from .services import process_bot_input
+from .report_services import process_report_bot_input
 from .rest_models import PredictArgs
 
 
 def process_prediction(data):
     obj = PredictArgs(**data)
-    data = process_bot_input(obj)
+    if obj.botId == 'report':
+        data = process_report_bot_input(obj)
+    else:
+        data = process_bot_input(obj)
+   # data = process_bot_input(obj)
     return data
-
